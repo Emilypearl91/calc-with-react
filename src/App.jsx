dccf5652c9fb7a27ce934a1c0af241986521d71d
Emily Pearl <Emilypearl9193@gmail.com>
2025-01-23 22:02:20 -0700

    calculator buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,38 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <button>9</button>
+          <button>8</button>
+          <button>7</button>
+        </div>
+        <div>
+          <button>6</button>
+          <button>5</button>
+          <button>4</button>
+        </div>
+        <div>
+          <button>3</button>
+          <button>2</button>
+          <button>1</button>
+        </div>
+        <div>
+          <button>0</button>
+        </div>
+      <div>
+        <button>clear</button>
+        <button>÷</button>
+        <button>x</button>
+        <button>-</button>
+        <button>+</button>
+        <button>=</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
